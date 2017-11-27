2d635e15423f8e28f564c25dc07c034dece6ab20
tscotce <tscotce@gmail.com>
2017-11-27 03:52:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function printBadges (names) {
 }
 
 function tailsNeverFails() {
-  let counter = 0;
+  let i = 0;
   while (Math.random() >= 0.5){
-    counter++;
+    i++;
   }
-  return `You got ${counter} tails in a row!`;
+  return `You got ${i} tails in a row!`;
 }
 
 function tailsNeverFails() {
