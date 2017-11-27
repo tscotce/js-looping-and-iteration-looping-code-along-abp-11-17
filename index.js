a954e7cb68d79037f95ec87b963abbd80637b576
tscotce <tscotce@gmail.com>
2017-11-27 03:48:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function printBadges (names) {
 
 function tailsNeverFails() {
   let counter = 0;
-  while (Math.random() > 0.5){
+  while (Math.random() >= 0.5){
     counter++;
   }
   return `You got ${counter} tails in a row!`;
