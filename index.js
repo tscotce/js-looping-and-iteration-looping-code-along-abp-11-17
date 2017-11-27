a68e2714ca0b3c88f75a33226c1e6abc11235bb4
tscotce <tscotce@gmail.com>
2017-11-27 03:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function tailsNeverFails () {
 // }
 
 function tailsNeverFails () {
-  return Math.random () <=1;
+  return Math.random () <1;
 }
 while (tailsNeverFails ()) {
   console.log (`You got ${[i]} tails in a row!`)
